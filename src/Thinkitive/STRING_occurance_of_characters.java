5fcf8a7c08491f11e1e224cc208b2dadebc052a0
Shubham Singh Bhadoria <71599208+bhadoria787@users.noreply.github.com>
2024-04-28 15:08:37 +0530

    STRING_occurance_of_characters.java

--- src/Thinkitive/STRING_occurance_of_characters.java
+++ src/Thinkitive/STRING_occurance_of_characters.java
@@ -10,21 +10,13 @@ public class STRING_occurance_of_characters
 	public static void main(String[] args) 
 	{
 		String str = "thinkitive";
+		// Create a map to store character frequencies
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
 
-		Map<Character, Integer> countOcc = new LinkedHashMap<>();
-
-		for (Character c : str.toCharArray()) 
-		{
-			if (countOcc.containsKey(c)) 
-			{
-				countOcc.put(c, countOcc.get(c) + 1);
-			} 
-			else 
-			{
-				countOcc.put(c, 1);
-			}
-
-		}
-		System.out.println(countOcc);
+        // Iterate through the string to count occurrences of each character
+        for (char c : str.toCharArray()) {
+            charFrequencyMap.put(c, charFrequencyMap.getOrDefault(c, 0) + 1);
+        }
+		System.out.println(charFrequencyMap);
 	}
 }
