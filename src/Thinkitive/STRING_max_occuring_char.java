0b1f7ebf107373b3f79a286b9cf4579d14324f90
Shubham Singh Bhadoria <71599208+bhadoria787@users.noreply.github.com>
2024-04-28 15:14:03 +0530

    Update STRING_max_occuring_char.java

--- src/Thinkitive/STRING_max_occuring_char.java
+++ src/Thinkitive/STRING_max_occuring_char.java
@@ -5,48 +5,32 @@ import java.util.Scanner;
 
 public class STRING_max_occuring_char {
 
-	static String maxOccuringChar(String str) 
-	{
+	public static char getMaxOccurringCharacter(String str) {
+        // Create a map to store character frequencies
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
 
-		int count[] = new int[256];
-		int max = 0;
-		char character = ' ';
-		String stfd = "thinkitive";
-		int counting[] = new int[256];
-		for (int i = 0; i < stfd.length(); i++)
-		{
-			//System.out.println("str.charAt(i): "+stfd.charAt(i));
-			counting[stfd.charAt(i)]++;//to store count of each letter of string
-		}
-		for (int i = 0; i < stfd.length(); i++)
-		{
-			System.out.println(stfd.charAt(i)+":  "+counting[stfd.charAt(i)]);
-		}
-		
-		
-		for (int i = 0; i < str.length(); i++)
-		{
-			//System.out.println("str.charAt(i): "+str.charAt(i));
-			count[str.charAt(i)]++;//to store count of each letter of string
-		}
-		
-		for (int i = 0; i < str.length(); i++) 
-		{
-			if (max < count[str.charAt(i)]) 
-			{
-				max = count[str.charAt(i)];
-				character = str.charAt(i);
-			}
+        // Iterate through the string to count occurrences of each character
+        for (char c : str.toCharArray()) {
+            charFrequencyMap.put(c, charFrequencyMap.getOrDefault(c, 0) + 1);
+        }
 
-		}
+        // Find the character with the maximum frequency
+        char maxChar = '\0';
+        int maxFrequency = Integer.MIN_VALUE;
+        for (Map.Entry<Character, Integer> entry : charFrequencyMap.entrySet()) {
+            if (entry.getValue() > maxFrequency) {
+                maxFrequency = entry.getValue();
+                maxChar = entry.getKey();
+            }
+        }
 
-		return "Here "+character+" is occures "+max+" time";
-	}
+        return maxChar;
+    }
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = "shubham singh bhadoria";
-		System.out.println(maxOccuringChar(str));
+		System.out.println(getMaxOccurringCharacter(str));
 		
 		
 	}
