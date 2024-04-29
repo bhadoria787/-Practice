a11ad7e2e08a73320231207fcb40857dae8e499a
Shubham Singh Bhadoria <71599208+bhadoria787@users.noreply.github.com>
2024-04-30 02:45:49 +0530

    Update Occurance_of_2_form_1to_100.java

--- src/idz_digital/Occurance_of_2_form_1to_100.java
+++ src/idz_digital/Occurance_of_2_form_1to_100.java
@@ -1,35 +1,22 @@
 package idz_digital;
 
 public class Occurance_of_2_form_1to_100 {
-	
-	static int number0f2s(int n)  
-	{ 
-	    int count = 0; 
-	    while (n > 0)  
-	    { 
-	        if (n % 10 == 2) 
-	        count++; 
-	  
-	        n = n / 10; 
-	    } 
-	    return count; 
-	} 
-  
-	static int numberOf2sinRange(int n)  
-	{ 
-	      
-	    // Initialize result 
-	    int count = 0;  
-	  
-	    // Count 2's in every number  
-	    // from 2 to n 
-	    for (int i = 2; i <= n; i++) 
-	    count += number0f2s(i); 
-	  
-	    return count; 
-	}
-	
 	public static void main(String[] args) {
-		System.out.println(numberOf2sinRange(10));
-	}
+        int count = 0;
+        for (int i = 1; i <= 100; i++) {
+            count += countOccurrences(i, 2);
+        }
+        System.out.println("Number of occurrences of digit 2 from 1 to 100: " + count);
+    }
+
+    private static int countOccurrences(int number, int digit) {
+        int count = 0;
+        while (number > 0) {
+            if (number % 10 == digit) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
+    }
 }
