ef1706e39116fe340241482020095ce907f123f3
Shubham Singh Bhadoria <71599208+bhadoria787@users.noreply.github.com>
2024-04-30 02:59:49 +0530

    Update STRING_palindrome_or_not.java

--- src/Thinkitive/STRING_palindrome_or_not.java
+++ src/Thinkitive/STRING_palindrome_or_not.java
@@ -2,60 +2,28 @@ package Thinkitive;
 
 public class STRING_palindrome_or_not 
 {
-	public static void ispalindrome(String str) 
-	{
-		char arr[] = str.toCharArray();
-		int size = arr.length;
-		char revarr[] = new char[size];
-		int count = 0;
-		int j=0;
-		
-		for (int i = size - 1; i >= 0; i--) //To reverse string
-		{
-			revarr[j] = arr[i];
-			j++;
-		}
-
-		for (j = 0; j < size; j++) //To count similar no of character
-		{
-			if (arr[j] != revarr[j]) 
-			{
-				++count;
-			}
-		}
-		
-		if (count == size) 
-		{
-			System.out.println("string is planidrome");
-		} else 
-		{
-			System.out.println("string is not aplanidrome");
-		}
-	}
-	
-	public static void PalindromeOrNot(String str)// Another way using string equalsIgnoreCase function
-	{
-		String revs = "";
-		char arr[] = str.toCharArray();
-		for (int i = arr.length - 1; i >= 0; i--) //To reverse string
-		{
-			revs = revs + arr[i];
-		}
-		
-		if (revs.equalsIgnoreCase(str)) 
-		{
-			System.out.println("second method: string is planidrome");
-		} else 
-		{
-			System.out.println("second method: string is not aplanidrome");
-		}
-	}
-
-	public static void main(String[] args) 
-	{
-		ispalindrome("makookam");
-		PalindromeOrNot("makookam");
-		
-	}
+	public static void main(String[] args) {
+        String str = "radar"; // Example string
+        if (isPalindrome(str)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+    
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
 }
 
